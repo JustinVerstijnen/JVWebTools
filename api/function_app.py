141abe4daa8f35165b1fe7ab9b1b2fa8ba8fefa1
Justin Verstijnen <info@justinverstijnen.nl>
2026-01-01 23:38:09 +0100

    Add files via upload

--- api/function_app.py
+++ api/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 
 # Single Functions app for the Static Web App.
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # Register tool routes (namespaced by folder/tool name)
 from DNSMegaTool.routes import register as register_DNSMegaTool
